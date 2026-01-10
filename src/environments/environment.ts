6ffdc39a964f9c03013aa3f1c1981e8c3788fffc
Yougal kumar <kyougal70@gmail.com>
2026-01-10 15:16:16 +0530

    change backend url from localhost

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'http://localhost:4000',
-  // apiUrl: 'https://subkuch-back.onrender.com',
+  // apiUrl: 'http://localhost:4000',
+  apiUrl: 'https://subkuch-back.onrender.com',
 };
